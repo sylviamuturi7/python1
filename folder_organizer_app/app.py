7f6860d07f0f18b054f67cc86e03ce4d69e5f7aa
sylviamuturi7 <sylviamuturi7@gmail.com>
2026-02-12 11:22:33 +0300

    Thursday

--- folder_organizer_app/app.py
+++ folder_organizer_app/app.py
@@ -3,7 +3,8 @@
 #main()
 
 from user_folder import get_user_folder
-from file_sys import list_folder_files
+from file_sys import list_folder_files,move_files
+import os
 
 IMAGES=["jpg","jpeg","png"]
 MUSIC=["mp3","wav"]
@@ -19,4 +20,29 @@ def main():
 
     for item in items:
         print("single item is",item)
+        split_item=os.path.splitext(item)
+        #print("split item is",split_item)
+        #print("first element",split_item[0])
+        #print("second element",split_item[1])
+        extension=split_item[1]
+        folder_name="other"
+
+        if extension in IMAGES:
+            print("f is file (item) its an image")
+            folder_name="images"
+        elif extension in MUSIC:
+            print("f is file (item) its a music")
+            folder_name="music"
+        elif extension in PDF:
+            print("f is file (item) its a pdf")
+            folder_name="pdf"
+        elif extension in ZIP:
+            print("f is file (item) its a zip")
+            folder_name="zip"
+        else:
+            print("f is file (item) its in others or unknown")
+            folder_name="other"
+    print("The folder name is",folder_name)
+    move_files(working_folder,item,folder_name)
+        
 main()
--- folder_organizer_app/file_sys.py
+++ folder_organizer_app/file_sys.py
@@ -1,6 +1,21 @@
 import os
+import shutil
+
 def list_folder_files(folder_path):
     files=os.listdir(folder_path)
     #print(files)
     return files
    #listing all files after inputing the folder path 
+
+def move_files(working_dir,file_name,folder_name):
+    destination_path=os.path.join(working_dir,folder_name)
+    #destination path exists if it doesnt create one
+    source=os.path.join(working_dir,file_name)
+    print("Moving file to",destination_path)
+
+    isdir=os.path.isdir(destination_path)
+
+    #creating the folder if it doesnt exist
+    if not isdir:
+        os.mkdir(destination_path)
+    shutil.move(src=source,dst=destination_path)
